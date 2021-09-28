872290cfcf8405eee61d375df2a7792392d6cd45
billfern23 <billfern23@gmail.com>
2021-09-27 22:50:52 -0400

    added post route

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 //assignment 1 that is working
 const express = require("express");
 const mongoose = require("mongoose")
-const customerModel = require("./models/Customer.js")
+const customerModel = require('./models/Customer.js')
 require('dotenv').config({ path: 'config/keys.env'})
 const PORT = 3000;
 
 
 app = express();
-
+//user json
+app.use(express.json());
+//creating a new user
+app.post("/register",(req,res)=>{
+    //instantiate the document you created in model
+   const customer = new customerModel(req.body)
+   customer.save()
+   .then((newCustomer)=>{
+      res.json({
+          message: "the customer successfully added",
+          data: newCustomer
+      })
+   })
+   .catch((err)=>{
+       console.log(`Errod ${err}`)
+   })
+})
 app.listen(process.env.PORT,() =>{
     console.log(`Server is up and running ${process.env.Port}`)
     mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING)
