b85586063f994f6cf1c6a08c81ef4f04d912a7d3
billfern23 <billfern23@gmail.com>
2021-09-27 21:54:52 -0400

    setup connection to mongoose

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose")
 require('dotenv').config({ path: 'config/keys.env'})
 const PORT = 3000;
 
 app = express();
 app.listen(process.env.PORT,() =>{
     console.log(`Server is up and running ${process.env.Port}`)
-    
+    mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING)
+    .then(()=>{
+        console.log(`Database connection successfull`)
+    })
+    .catch(err=>{
+        console.log(`Error: ${err}`)
+    })
+
+
 })
