734cf7168e0062b16bbfff4aedd47ea718193276
billfern23 <billfern23@gmail.com>
2021-10-10 03:06:19 -0400

    heroku porblems

--- controllers/ProductsControllers.js
+++ controllers/ProductsControllers.js
@@ -5,15 +5,15 @@ const Productservices = require('../services/Productsservices.js')
 
 //Product Schema
 // add product
-router.post("/addproduct",Productservices.addnewproduct );
+router.post("/addproduct",ProductServices.addnewproduct );
 
 //2. retrieve all product
 //4. retrieve producst belonging to specific product category
 //5. retrieve best selleter
-router.get("/",Productservices.retrieveandfilterProducts )
+router.get("/",ProductServices.retrieveandfilterProducts )
 
 //3. filter to get all categories
-router.get("/categories",Productservices.showAllcategories)
+router.get("/categories",ProductServices.showAllcategories)
 
 
 
