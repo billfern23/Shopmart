affbcf96c9c4bd8993fb2e875128aeb6c64c11fa
billfern23 <billfern23@gmail.com>
2021-10-10 02:34:00 -0400

    readme update, product service validation

--- Readme.md
+++ Readme.md
@@ -5,29 +5,144 @@ This is an API that does the following:
 
 DISCLAIMER: **Inorder to use the api, you have to use these endpoints with inputs the proper case**
 
+
+**QUICK LINKS**
+Customer:
+1)To Register a new Customer: https://shopmart24.herokuapp.com/customer/register
+2)To Retrieve a customer using ID: https://shopmart24.herokuapp.com/customer/:id
+
+Products:
+1)Add new product to database: https://shopmart24.herokuapp.com/products/addproduct
+2)Retrieve all products from database: https://shopmart24.herokuapp.com/products
+3)Retrieves All categories from Database: https://shopmart24.herokuapp.com/products/categories
+4)Retrieves All products from a specific category: https://shopmart24.herokuapp.com/products?category=categoryname
+5)Retrieve products that are Best sellers: https://shopmart24.herokuapp.com/products?bestseller=yes
+6)Retrieve a product by ID:  https://shopmart24.herokuapp.com/products/:id
+
+
 CUSTOMERS
 1) To Register a new Customer: 
-    POST METHOD
+    
+    METHOD: POST
+
     https://shopmart24.herokuapp.com/customer/register
 
     required Keys in body: 
-    {
-        fname : String datatype, 
-        lname: String datatype,
-        email: : String datatype, 
-        password: String Datatype,
-    }
+      Reference guide:
+                        {
+                            fname : String datatype, 
+                            lname: String datatype,
+                            email: : String datatype, 
+                            password: String Datatype,
+                                                         }
+            
 
-    Optional keys in body: phone : [Numbers] (Array of numbers datatype) 
+    Optional keys in body:          phone : [Numbers] 
+                                (Array of numbers datatype) 
+    Example:
+                {
+                    fname : "test", 
+                    lname: "test",
+                    email: : "test",
+                    password: "test",
+                    number:  [12345678,1234567]
+                                                  }
 
 2) To Retrieve a customer using ID:
-    GET METHOD
+    
+    METHOD: GET
+
     https://shopmart24.herokuapp.com/customer/:id
 
     id is a required parameter, otherwise customer wont be retrieved.
-    Replace ':id' with known customer ID.
+    Replace ':id' with known customer ID. It has to be the same id, length, sequence all of it.
     **You would need the exact ID for this endpoint to retrieve a customer.
 
 Products
 
-3)
+1) To add a new Product to shopmart Database:
+    
+    METHOD: GET
+
+    https://shopmart24.herokuapp.com/products/addproduct
+  
+
+    Required keys in the body:
+    Reference guide:
+                        {
+                            "name": String datatype,
+                            "price": Number datatype,
+                            "category" : String datatype,
+                            "bestSeller": Boolean datatype 
+                                                               }
+    Optional paramters:
+                {
+                    description: String datatype,
+                    quantity: Number Datatype,
+                    url: String datatype
+
+                                                }
+                  
+
+    Example
+                {
+                        "name": "test", 
+                        "price": 0,
+                        "description": "test", 
+                        "category" :"test", 
+                        "quantity": 9,
+                        "bestSeller": true, 
+                        "picurl":"test" 
+
+                            }
+                            Boolean values: true/false
+2) Retrieve all products from database:
+      
+        METHOD: GET
+
+        https://shopmart24.herokuapp.com/products
+    
+    
+    This will give you a list of all products from the database
+
+
+
+3) Retrieves All categories from Database:
+        
+        METHOD: GET
+
+        https://shopmart24.herokuapp.com/products/categories
+
+4) Retrieves All products from a specific category:
+    
+        METHOD: GET
+        
+        https://shopmart24.herokuapp.com/products?category=categoryname
+
+        Method uses a query field a query field called cateogry, replace categoryname with the category of products you want to retrieve
+        ****DISCLAIMER: The categoryname must be exactly the same way it is stored in the database ie, case and spelling sensitive****
+
+        Example:
+        https://shopmart24.herokuapp.com/products?category=Table
+
+        not table, Table.
+
+5) Retrieve products that are Best sellers
+    Method: Get
+    
+    https://shopmart24.herokuapp.com/products?bestseller=yes
+
+
+    This method uses a query field called bestseller, it takes parameters yes or | Yes.
+    **Both are case sensitive and spelling sensitive**
+
+
+6)Retrieve a product by ID:
+    
+    Method: Get
+
+    https://shopmart24.herokuapp.com/products/:id
+
+    id is a required parameter, otherwise product wont be retrieved.
+    Replace ':id' with known product ID.
+    **You would need the exact ID for this endpoint to retrieve a product.**
--- services/Productsservices.js
+++ services/Productsservices.js
@@ -173,7 +173,7 @@ exports.retrieveandfilterProducts = (req, res) =>{
                 })
             }
             else {
-                res.json({
+                res.status(404).json({
                     message: `Nothing found, please check category list with /categories, endpoint, case sensitive `,
               
                 })
@@ -191,7 +191,7 @@ exports.retrieveandfilterProducts = (req, res) =>{
     }
     
     else if(req.query.bestseller){
-        if(req.query.bestseller == "yes" || req.query.bestseller == "Yes"){
+        if(req.query.bestseller == "yes" || req.query.bestseller == "YES"){
     
             productModel.find()
             .where('bestSeller').equals(true)
@@ -211,7 +211,7 @@ exports.retrieveandfilterProducts = (req, res) =>{
         }
     
         else {
-            res.json({
+            res.status(404).json({
                 message: `Please type yes or Yes`
             })
         }
@@ -236,7 +236,7 @@ exports.retrieveandfilterProducts = (req, res) =>{
     }
 };
 
-
+//all categories
 exports.showAllcategories = (req, res) => {
     productModel.find({}).distinct('category')
     .then((products)=>{
@@ -254,6 +254,7 @@ exports.showAllcategories = (req, res) => {
 };
 
 
+//find product by id
 exports.getAProduct = (req, res) =>{
     productModel.findById(req.params.id)
     .then(Product=>{
@@ -265,7 +266,7 @@ exports.getAProduct = (req, res) =>{
             })
         }
         else {
-            res.status(400).json({
+            res.status(404).json({
                 message: `No Product with id ${req.params.id}`
             })
         }
@@ -274,7 +275,7 @@ exports.getAProduct = (req, res) =>{
     .catch(err=>{
         if(err.name == "CastError") {
             res.status(404).json({
-                message: `Id format is incorrect, please fix, wrong number of characters in id`
+                message: `Id format is incorrect, please fix, Its either too long or too short or is not an id`
             })
         }
         else {
@@ -287,46 +288,152 @@ exports.getAProduct = (req, res) =>{
         })
 };
 
-
+//update one
 exports.updateAProduct = (req, res) =>{
-    const {_id} = req.body;
+    const {_id, name, price, description, category, quantity, bestSeller, picurl} = req.body;
     if(_id){
         res.status(404).json({
             message: `ids cannot be updated, please remove them from body`
         })
     } else {
-        productModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        .then((Product) =>{
-            if(Product){
-
-                res.json({
-                    message: `Product with the id ${req.params.id} was updated`,
-                    data: Product
-                })
-            }
-            else {
-                res.status(404).json({
-                        message: `Product with id ${req.params.id} does not exist`
-                })
-            }
-
-        })
-        .catch((err)=>{
-            if(err.name == "CastError") {
-                res.status(404).json({
-                    message: `Id format is incorrect, please fix, wrong number of characters in id`
-                })
-            }
-            else {
+        let errorListupdate = {
+            nameCheck: "",
+            priceCheck: "",
+            descriptionCheck: "",
+            categorycheck: "",
+            quantitycheck: "",
+            bestSellercheck: "",
+            picurlcheck: ""
     
-                res.status(500).json({
-                    messsage:  err
-                })
-            }
-    
-        })
+        }
+        let checker = "true"
         
-    }
+        if(name != undefined){
+            if(typeof(name)!== "string"){
+               
+                errorListupdate.nameCheck = ` Name has to be a string Type`
+                checker = false
+            }
+            else if(name == "") {
+                errorListupdate.nameCheck = `Name cannot be empty`
+                checker = false
+            }
+        }
+        if(!checker){
+            res.json({
+                message: `Some parameters need fixing before you update, otherwise take them out`,
+                paramters: errorListupdate
+            })
+        }
+
+         if(price != undefined){
+            if(typeof(price)!== "number"){
+               
+                errorListupdate.priceCheck = ` Price has to be a number Type`
+                checker = false
+            }
+           
+        }
+        if(description != undefined){
+            if(typeof(description)!== "string"){
+               
+                errorListupdate.descriptionCheck = ` Description has to be a string Type but can be replaced to empty`
+                checker = false
+            }
+        }
+
+
+        if(category != undefined){
+            if(typeof(category)!== "string"){
+               
+                errorListupdate.categorycheck = ` Category has to be a string Type`
+                checker = false
+            }
+            else if(category == "") {
+                errorListupdate.categorycheck = `Category cannot be empty`
+                checker = false
+            }
+        }
+        
+        if(quantity != undefined){
+            if(typeof(quantity)!== "number"){
+               
+                errorListupdate.quantitycheck = ` Convert quantity to 0 to show remove`
+                checker = false
+            }
+           
+        }
+
+        if(bestSeller != undefined){
+            if(typeof(bestSeller)!== "boolean"){
+               
+                errorListupdate.bestsellercheck = ` Bestseller has to be a Boolean Type, true/false`
+                checker = false
+            }
+        
+        }
+
+        if(picurl != undefined){
+            if(typeof(picurl)!== "string"){
+               
+                errorListupdate.picurlcheck = ` Pic url has to be a string Type but can be replaced to empty`
+                checker = false
+            }
+        }
+
+
+        for (const property in errorListupdate) {
+            if (!errorListupdate[property]) {
+              delete errorListupdate[property];
+            }
+          }
+           
+        
+
+
+
+        if(!checker){
+            res.json({
+                message: `Some parameters need fixing before you update, otherwise take them out`,
+                paramters: errorListupdate
+            })
+        }
+
+        else {
+
+            productModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
+            .then((Product) =>{
+                if(Product){
+    
+                    res.json({
+                        message: `Product with the id ${req.params.id} was updated`,
+                        data: Product
+                    })
+                }
+                else {
+                    res.status(404).json({
+                            message: `Product with id ${req.params.id} does not exist`
+                    })
+                }
+    
+            })
+            .catch((err)=>{
+                if(err.name == "CastError") {
+                    res.status(404).json({
+                        message: `Id format is incorrect, please fix, wrong number of characters in id`
+                    })
+                }
+                else {
+        
+                    res.status(500).json({
+                        messsage:  err
+                    })
+                }
+        
+            })
+            
+        }
+        }
 };
 
 
