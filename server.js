4337e3a36debb19493d9a7b1ab3153ce315c13d1
Billy Fernandes <56287260+billfern23@users.noreply.github.com>
2021-11-11 18:49:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ if (process.env.NODE_ENV != "production") {
 }
 
 const app = express();
+const allowlist = ['http://localhost:3000', 'https://eloquent-feynman-3510c7.netlify.app/']
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(cors(corsOptionsDelegate
 //user json
 app.use(express.json());
 
